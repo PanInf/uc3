15dcc302450a55427a8d124e9cf5a3de72312959
PanInf <paninf@paninf.pl>
2026-02-02 11:57:40 +0100

    Dodanie pliku daabase.js tworzacego baze z 1 tabela i 2 inserty

---
+++ pra0/backend/database.js
@@ -0,0 +1,32 @@
+import Database from 'better-sqlite3'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const dbPath = path.join(__dirname, '../database', process.env.DB_NAME)
+
+
+
+const db = new Database(dbPath)
+
+db.exec(`
+    CREATE TABLE IF NOT EXISTS Expenses (
+    ExpenseID INTEGER PRIMARY KEY AUTOINCREMENT,
+    category_id INTEGER NOT NULL,
+    Name TEXT,
+    Description TEXT,
+    Amount REAL)
+    `)
+
+db.exec(`
+    INSERT INTO Expenses (category_id, Name, Description, Amount)
+    VALUES (1, 'Kawa', 'Poranna kawa na mieście', 15.50);
+    `)
+
+db.exec(`
+    INSERT INTO Expenses (category_id, Name, Description, Amount)
+    VALUES (2, 'Obiad', 'Lunch w pracy', 32.00);
+    `)
