08f43e2c058a339a11c0472ee958dada670dde5f
Maksimtsa <maksim.tsarik2000@gmail.com>
2026-02-02 11:58:48 +0100

    Dodanie pliku database.js tworzacego baze z tabela i 2 inserty

---
+++ pra2/backend/backend.js
@@ -0,0 +1,23 @@
+import Database from 'better-sqlite3';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const dbPath = path.join(__dirname, '../database', process.env.DB_NAME);
+
+
+const db = new Database(dbPath);
+
+db.exec(
+    'CREATE TABLE IF NOT EXISTS Expenses (ExpenseID INTEGER PRIMARY KEY,CategoryID INTEGER,Name VARCHAR(50),Description VARCHAR(200),Amount REAL,Date TEXT)'
+);
+
+db.exec(`
+  INSERT INTO Expenses (CategoryID, Name, Description, Amount, Date)
+  VALUES (1, 'ww', 'www', 25, '2026-02-02')
+`);
+
+const rows = db.prepare(`SELECT * FROM Expenses`).all();
+console.log(rows);
